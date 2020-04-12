f8c06eb4e8566b0d8b13fd996ff5bd55e2e3f3e1
verplancken <42614785+verplancken@users.noreply.github.com>
2020-04-12 15:48:14 -0500

    app

---
+++ 06-twittor/sw.js
@@ -0,0 +1,68 @@
+//importacion
+importScripts('js/sw-utils.js');
+
+const STATIC_CHACHE= 'static-v1';
+const DYNAMIC_CHACHE= 'dinamic-v1';
+const INMUTABLE_CHACHE= 'inmutable-v1';
+
+const APP_SHELL = [
+	'/',
+	'index.html',
+	'css/style.css',
+	'img/favicon.ico',
+	'img/avatars/hulk.jpg',
+	'img/avatars/ironman.jpg',
+	'img/avatars/spiderman.jpg',
+	'img/avatars/thor.jpg',
+	'js/app.js',
+	'js/sw-utils.js'
+];
+
+const APP_SHELL_INMUTABLE = [
+	'https://fonts.googleapis.com/css?family=Quicksand:300,400',
+	'https://fonts.googleapis.com/css?family=Lato:400,300',
+	'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+	'css/animate.css',
+	'js/libs/jquery.js'
+];
+
+self.addEventListener('install', e =>{
+	const cacheStatic = caches.open( STATIC_CHACHE ).then(cache =>
+		cache.addAll(APP_SHELL));
+
+	const cacheInmutable = caches.open( INMUTABLE_CHACHE ).then(cache =>
+		cache.addAll(APP_SHELL_INMUTABLE));
+
+	e.waitUntil( Promise.all([cacheStatic, cacheInmutable ]) );
+});
+
+self.addEventListener('active', e =>{
+	const respuesta = caches.keys().then( keys => {
+
+        keys.forEach( key => {
+
+            // static-v4
+            if (  key !== STATIC_CHACHE && key.includes('static') ) {
+                return caches.delete(key);
+            }
+
+        });
+
+    });
+    e.waitUntil( respuesta );
+});
+
+const respuesta = self.addEventListener('fetch', e =>{
+	caches.match( e.request ).then( resp =>{
+		if(res){
+			return res;
+		}else{
+			return fecth( e.request ).then(resp =>{
+				return actualizaCacheDinamico( DYNAMIC_CHACHE, e.request, newRes );
+			});
+			
+		}
+		
+	});
+	e.respondWith(respuesta);
+});
